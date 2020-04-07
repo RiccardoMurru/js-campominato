45acdc5d60805c83a1d546ba839f2ebf4e8570fa
Riccardo Murru <riccardo.mr@gmail.com>
2020-04-07 16:13:13 +0200

    add. main loop and set difficulty

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,42 @@
  */
 
 
+/**
+ * Inizializzazione
+ */
+
 var bombs = [];
 var newBomb = 0;
 var attempts = [];
+var possibilities = 0;
+var difficulty = -1;
+
+
+// chiedere all'utente la difficoltà finché non inserisce un numero valido (0, 1, 2)
+while (!isValidNumber(difficulty, -1, 2)) {
+    difficulty = parseInt( prompt (' Inserisci: \n 0 per facile \n 1 per normale \n 2 per difficile'));    
+}
+
+// se numero inserito è valido settare il numero massimo di possibilità
+switch (difficulty) {
+    case 0:
+        possibilities = 84;
+        break;
+    case 1:
+        possibilities = 64;
+        break;
+    case 2:
+        possibilities = 34;
+        break;
+}
+
 
 // inserire numeri random nell'array
 var i = 0;
 
 while (i < 16) {
     newBomb = getRandomNumber(1, 100);
+
     if (!isInArray(bombs, newBomb)) {
         bombs.push(newBomb);
         i++;
@@ -36,40 +63,38 @@ while (i < 16) {
 
 console.log(bombs.sort());
 
+
+/**
+ * Loop principale
+ */
+
+
+
 // chiedere di inserire numeri all'utente
-var newAttempt = parseInt( prompt('Inserisci un numero da 1 a 100'));
-attempts.push(newAttempt);
+var attemptsCounter = 0;
+var newAttempt = 0;
 
-// verificare se il numero inserito è all'interno degli array bombe o tentativi
-var j = 1;
-while (!isInArray(bombs, newAttempt)) {
+while ( (attempts.length < possibilities) && (!isInArray(bombs, newAttempt)) ) {
+    newAttempt = parseInt(prompt('Inserisci un numero da 1 a 100'));
+    
+    // verificare se il numero inserito è valido e se è all'interno degli array bombe o tentativi
+    validateNumber(newAttempt);
+    
+    // stampa del risultato
+   switch (attempts.length == possibilities) {
+       case true:
+           console.log('Hai vinto, numero tentativi', attemptsCounter);
+           break;
+   }   
+  
+}       
 
-    switch (isInArray(attempts, newAttempt)) {
-        case true:
-            newAttempt = parseInt( prompt('Non puoi inserire lo stesso numero più volte, riprova'));
-            console.log(attempts);
-            
-            break;
-        case false:
-            newAttempt = parseInt( prompt('Complimenti, inserisci un altro numero da 1 a 100'));
-            attempts.push(newAttempt);
-            console.log(attempts);
-            
-            break;
-    }
-    j++;
-}
 
-// stampa del risultato
+/**
+ *  Funzioni 
+ */
 
-switch (j < 84) {
-    case true:
-        console.log('Peccato, hai perso. Numero di tentativi: ', j);
-        break;
-    case false:
-        console.log('Complimenti hai vinto!');
-        break;
-}
+
 
 // funzione per generare numeri casuali da 1 a 100
 function getRandomNumber (min, max) {
@@ -81,4 +106,26 @@ function getRandomNumber (min, max) {
 function isInArray (array, number) {
     return (array.includes(number));
     
+}
+
+// funzione per stabilire se un numero è valido
+function isValidNumber (number, min, max) {
+    return ((number != isNaN) && (number > min) && (number <= max));
+}
+
+// funzione per validare il numero inserito dall'utente
+function validateNumber(number) {
+    if (isInArray(bombs, number)) {
+        console.log('Hai perso, numero di tentativi ', attemptsCounter);
+        
+    } else if (isInArray(attempts, number)) {
+        alert('Numero già inserito, riprova');
+
+    } else if (!isValidNumber(number, 0, 100)) {
+        alert('Numero non valido, riprova');
+
+    } else if (!isInArray(attempts, number)) {
+        attempts.push(number);
+        attemptsCounter++;
+    }
 }
